c31a1733b3eede6bae1db3652971f301c6ebd9d3
nfamula <nfamula@gmail.com>
2016-01-24 03:49:33 +0000

    exercise 7

--- sinatra-skeleton-master/app/actions.rb
+++ sinatra-skeleton-master/app/actions.rb
@@ -1,5 +1,21 @@
+require 'pry'
 # Homepage (Root path)
 get '/messages' do
   @messages = Message.all
   erb :'messages/index'
+end
+
+post '/messages' do
+  @message = Message.new(
+  	title: params[:title],
+  	content: params[:content],
+  	author: params[:author]
+  	)
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'	
 end
